88f7c7dda9ac9412f617376579adbe76650a2b32
tunganh123 <louistunganh@gmail.com>
2023-05-31 16:25:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ require("dotenv").config();
 const cors = require("cors");
 const corsoption = {
   // origin: [process.env.URL_USER, process.env.URL_ADMIN],
-  origin: ["https://asm-3-user.vercel.app,"http://localhost:3000"]
+  //origin: ["https://asm-3-user.vercel.app,"http://localhost:3000"]
+  origin: "http://localhost:3000",
   credentials: true,
 };
 const mongoose = require("mongoose");
