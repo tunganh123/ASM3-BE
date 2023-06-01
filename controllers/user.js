bd4fd348ec29b1254f1a7b67c11e10d5808ae0e9
tunganh123 <louistunganh@gmail.com>
2023-06-01 10:00:12 +0700

    set cookie

--- controllers/user.js
+++ controllers/user.js
@@ -54,9 +54,9 @@ exports.login = async (req, res) => {
     //create cookie in client
     res.cookie("token", token, {
       path: "/",
-      httpOnly: false,
-      secure: false,
-      samesite: "strict",
+      sameSite: "none",
+      // secure: true,
+      httpOnly: true,
     });
     res.json({ token: token });
   } catch (error) {
