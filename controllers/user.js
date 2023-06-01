6bea67a514f7fe7788c642f74330b091c59b5e5f
tunganh123 <louistunganh@gmail.com>
2023-06-01 11:08:13 +0700

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -56,7 +56,7 @@ exports.login = async (req, res) => {
       // path: "/",
       // domain: "https://asm-3-be.vercel.app",
       httpOnly: true,
-      secure: true,
+      secure: false,
       sameSite: "none",
     });
     res.json({ token: token });
