e5f7f30b9d9244e8ab63d7beaa4d1323bbfa5eca
tunganh123 <louistunganh@gmail.com>
2023-06-02 15:02:50 +0700

    update origin

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const corsoption = {
   origin: [
     "https://asm-3-user.vercel.app",
     "https://mobile-ecommerce.onrender.com",
+    "https://asm3-admin.onrender.com",
   ],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
